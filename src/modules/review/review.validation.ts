9bb0d26e3a68c9070313b787a1fe7d4dd1fc0735
sayed725 <ssayed72533@gmail.com>
2026-03-28 21:02:56 +0600

    added review schema

---
+++ src/modules/review/review.validation.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const createReviewSchema = z.object({
+  rating: z.number().min(1).max(5),
+  comment: z.string().min(1),
+  orderId: z.string().uuid(),
+  itemId: z.string().uuid(),
+});
+
+export const updateReviewSchema = z.object({
+  rating: z.number().min(1).max(5).optional(),
+  comment: z.string().min(1).optional(),
+});
+
+export type CreateReviewPayload = z.infer<typeof createReviewSchema>;
+export type UpdateReviewPayload = z.infer<typeof updateReviewSchema>;
