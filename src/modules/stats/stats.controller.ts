852d5909cf8fcec5a5339cd593fcceec375ae90a
sayed725 <ssayed72533@gmail.com>
2026-04-01 11:25:53 +0600

    added admin sttats controller

--- src/modules/stats/stats.controller.ts
+++ src/modules/stats/stats.controller.ts
@@ -3,7 +3,13 @@ import { asyncHandler } from "../../middlewares";
 import { statsServices } from "./stats.service";
 
 const getAdminStats = asyncHandler(async (req: Request, res: Response) => {
- 
+  const result = await statsServices.getAdminStats();
+
+  res.status(200).json({
+    success: true,
+    message: "Admin statistics retrieved successfully",
+    data: result,
+  });
 });
 
 export const statsControllers = {
